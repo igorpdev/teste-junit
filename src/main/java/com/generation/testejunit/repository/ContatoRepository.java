8dffa1dae28b1ccfbd27386ff214644d6004fd69
igorpdev <79109692+igorpdev@users.noreply.github.com>
2021-04-08 17:03:22 -0300

    teste unitario controller

--- src/main/java/com/generation/testejunit/repository/ContatoRepository.java
+++ src/main/java/com/generation/testejunit/repository/ContatoRepository.java
@@ -1,9 +1,14 @@
 package com.generation.testejunit.repository;
 
+import java.util.List;
+
 import com.generation.testejunit.model.ContatoModel;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContatoRepository extends JpaRepository<ContatoModel, Long> {
+
+    public ContatoModel findFirstByNome(String nome);
+	public List<ContatoModel> findAllByNomeIgnoreCaseContaining(String nome);
 
 }
